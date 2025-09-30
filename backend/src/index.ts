041c95fee6a27f739258abb2bd442bbd2adf8f72
FelipeMDantas <felipemdantas@terra.com.br>
2025-09-30 19:41:09 -0300

    [feat] Define the backend initial configuration

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+const express = require("express");
+import { Request, Response } from "express";
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
