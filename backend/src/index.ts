dad6aa18c15e23e86ab987c11e70215407b6c2a4
FelipeMDantas <felipemdantas@terra.com.br>
2025-10-04 19:58:10 -0300

    [feat] Define the startServer function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ const express = require("express");
 import { Request, Response } from "express";
 import { inngest } from "./inngest/client";
 import { functions as inngestFunctions } from "./inngest/functions";
+import { logger } from "./utils/logger";
 
 const app = express();
 const PORT = 3001;
@@ -12,6 +13,19 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const startServer = async () => {
+  try {
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+      logger.info(`Server is running on port ${PORT}`);
+      logger.info(
+        `Inngest endpoint available at http://localhost:${PORT}/api/inngest`
+      );
+    });
+  } catch (error) {
+    logger.error("Failed to start server:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
