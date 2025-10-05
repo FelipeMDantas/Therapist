b770b523664dbb36c5b7dc82a96ef1a7d04da886
FelipeMDantas <felipemdantas@terra.com.br>
2025-10-05 20:26:09 -0300

    [feat] Code the connectDB function

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,11 +3,15 @@ import { Request, Response } from "express";
 import { inngest } from "./inngest/client";
 import { functions as inngestFunctions } from "./inngest/functions";
 import { logger } from "./utils/logger";
+import { serve } from "inngest/express";
 
 const app = express();
-const PORT = 3001;
 
 app.use(express.json());
+app.use(
+  "/api/inngest",
+  serve({ client: inngest, functions: inngestFunctions })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
